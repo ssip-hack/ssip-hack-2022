fd5a1ad5d3f5392ca94da7a5f48524a34878bf88
Dhruv1563 <iamdhruv1563@gmail.com>
2022-09-15 11:29:12 -0700

    added the front end

---
+++ front_end/src/LoginForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class LoginForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Student Data</h1>
+      </div>
+    );
+  }
+}
