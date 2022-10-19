68dd9b0416aa4dca3fb6ce54e48ec66385b7dbb1
Malani Prince <malaniprince55@gmail.com>
2022-10-19 18:31:15 +0530

    wallah habibi
    normally update the remain data , in the origin

---
+++ Prince_Data/creating tables.sql
@@ -0,0 +1,47 @@
+create database ssip;
+use ssip;
+create table students(
+	first_name ,
+    middle_name ,
+    last_name ,
+    dob ,
+    age ,
+    orphan ,
+    adhaar_no ,
+    contact_no ,
+    mail_id ,
+    mother_tongue ,
+    CWSN ,
+    nationality ,
+    no_of_siblings ,
+    drug_addiction ,
+    father_name ,
+    mother_name ,
+    guardian_name ,
+    parents_contact ,
+    father_occupation ,
+    mother_occupation ,
+    faher_education ,
+    mother_education ,
+    family_income ,
+    debt ,
+    bpl_beneficiary ,
+    social_category ,
+    minority ,
+    school_number ,
+    admission_number ,
+    admission_ ,
+    academic_stream ,
+    status_in_prev_year ,
+    grade_in_prev_year ,
+    result_in_previous_year_type ,
+    out_of_school_child ,
+    appeared_in_previous_year_exam ,
+    scholarship_holder ,
+    suspension_days ,
+    dropped 
+);
+
+                                                                      
+insert into all_data (first_name ,middle_name ,last_name ,dob ,age ,orphan ,adhar_number,contect_number ,mail_id ,mother_tongue ,CWSN ,nationality ,no_of_siblings ,drug_addiction ,father_name ,mother_name ,guardian_name ,parents_contact ,father_occupation ,mother_occupation ,faher_education ,mother_education ,family_income ,debt ,bpl_beneficiary ,social_category ,minority ,school_number ,admission_number ,admission_date ,academic_stream ,status_in_prev_year ,grade_in_prev_year,result_in_previous_year_type,out_of_school_child,appeared_in_previous_year_exam ,scholarship_holder ,suspension_days,dropped )
+values ( 'malani' , 'prince' , 'hiteshbhai' ,'6-20-2002' , 12  ,  1 , 1 , 1 , 1 , 1 , 1 , 1, 1 , 1 , 1, 1 , 1 , 1, 1 , 1 , 1, 1 , 1 , 1, 1 , 1 , 1, 1 , 1 , 1, 1 , 1 , 1, 1 , 1 , 1, 1 , 1 , 1   ); 
