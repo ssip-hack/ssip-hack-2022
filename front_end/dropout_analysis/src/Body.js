3c001c9979b9eac7f401ba08c69d1dc8a45291d1
Malani Prince <malaniprince55@gmail.com>
2023-02-05 15:43:10 +0530

    deleted unnecessary clutter

--- front_end/dropout_analysis/src/Body.js
+++
@@ -1,7 +0,0 @@
-import { Component } from "react";
-
-export default class Body extends Component {
-  render() {
-    return;
-  }
-}
--- front_end/dropout_analysis/src/home.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-export default class home extends Component {
-    render() {
-        return (
-            <header></header>
-        );
-    }
-}
